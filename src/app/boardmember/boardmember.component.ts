7398e39011f0e35b7b485693ce0b5996197df303
vidya-aye <vidya.aye@gmail.com>
2025-03-26 13:08:33 +0530

    Add files via upload

---
+++ src/app/boardmember/boardmember.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boardmember',
+  templateUrl: './boardmember.component.html',
+  styleUrls: ['./boardmember.component.css']
+})
+export class BoardmemberComponent {
+
+}
---
+++ src/app/donatenow/donatenow.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donatenow',
+  templateUrl: './donatenow.component.html',
+  styleUrls: ['./donatenow.component.css']
+})
+export class DonatenowComponent {
+
+}
